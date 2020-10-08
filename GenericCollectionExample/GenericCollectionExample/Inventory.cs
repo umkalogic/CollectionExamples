b9551c4d50540b5b3aea36f46ecbc1331de70278
Lane P <p8@bk.ru>
2020-10-08 20:53:56 +0300

    Initial commit

---
+++ GenericCollectionExample/GenericCollectionExample/Inventory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace GenericCollectionExample
+{
+    public class Inventory
+    {
+        string name;
+        double cost;
+        int onhand;
+        public Inventory(string n, double c, int h)
+        {
+            name = n;
+            cost = c;
+            onhand = h;
+        }
+        public override string ToString()
+        {
+            return String.Format("{0,-10}Cost: {1,6:C}  On hand: {2}", name, cost, onhand);
+        }
+    }
+}
---
+++ GenericCollectionExample/GenericCollectionExample/MainClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCollectionExample
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            List<Inventory> inv = new List<Inventory>();
+            for (int i = 0; i < InitialData.Names.Length; i++)
+                inv.Add(new Inventory(InitialData.Names[i], InitialData.Costs[i], InitialData.Onhands[i]));
+            Console.WriteLine("Inventory list:");
+            foreach (Inventory i in inv)
+                Console.WriteLine("   " + i);
+            //inv.Sort();//Error without the IComparable
+            char[] chs = new char[] { 'B', 'K', 'L', 'A', 'E' };
+            List<char> charlist = new List<char>();
+            for (int i = 0; i < 5; i++)
+                charlist.Add(chs[i]);
+            Console.WriteLine("Char list:");
+            foreach (char i in charlist)
+                Console.Write("   " + i);
+            Console.WriteLine();
+            charlist.Sort();
+            foreach (char i in charlist)
+                Console.Write("   " + i);
+        }
+    }
+}
