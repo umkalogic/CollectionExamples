848bba62a87caef4a740009f3bba818d17cfa457
Svitlana Pustova Serebriakova <svitlana.pustova@gmail.com>
2020-10-08 23:23:20 +0300

    NonGenericCollectionEnumeratorExample

---
+++ CollectionEnumeratorExample/GenericCollectionExample/Inventory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+namespace NonGenericCollectionExample
+{
+    public class Inventory: IComparable
+    {
+        string name;
+        double cost;
+        int onhand;
+        public string Name { get { return name; } }
+        public Inventory(string n, double c, int h)
+        {
+            name = n;
+            cost = c;
+            onhand = h;
+        }
+        public static IComparer SortByName
+        { get { return (IComparer)new InventoryComparer(); } }
+
+        public int CompareTo(object obj)
+        {
+            Inventory t = obj as Inventory;
+            if (obj != null)
+                return this.cost.CompareTo(t.cost);
+            else
+                throw new ArgumentException("Parameter is not an Inventory!");
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0,-10}Cost: {1,6:C}  On hand: {2}", name, cost, onhand);
+        }
+    }
+    class InventoryComparer : IComparer
+    {
+        int IComparer.Compare(object o1, object o2)
+        {
+            Inventory t1 = o1 as Inventory;
+            Inventory t2 = o2 as Inventory;
+            if (t1 != null && t2 != null)
+                return string.Compare(t1.Name, t2.Name, StringComparison.Ordinal);
+                //Compare strings using ordinal (binary) sort rules.
+            else
+                throw new ArgumentException("Parameter is not an Inventory!");
+        }
+    }
+}
---
+++ CollectionEnumeratorExample/GenericCollectionExample/MainClass.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+
+namespace NonGenericCollectionExample
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            ArrayList inv = new ArrayList();
+            for (int i = 0; i < InitialData.Names.Length; i++)
+                inv.Add(new Inventory(InitialData.Names[i], InitialData.Costs[i], InitialData.Onhands[i]));
+            Console.WriteLine("Inventory list:");
+            // Use enumerator to access list
+            IEnumerator etrInv = inv.GetEnumerator();
+            while (etrInv.MoveNext())
+                Console.WriteLine(etrInv.Current + " ");
+            Console.WriteLine();
+
+            inv.Sort();//sorts by cost
+            Console.WriteLine("Sorted by cost:");
+            IEnumerator etrInv2 = inv.GetEnumerator();//error without this
+            //because the Sort method modifies the collection
+            while (etrInv2.MoveNext())
+                Console.WriteLine(etrInv2.Current + " ");
+            Console.WriteLine();
+
+            inv.Sort(Inventory.SortByName);
+            Console.WriteLine("Sorted by name:");
+            foreach (Inventory i in inv)
+                Console.WriteLine("   " + i);
+
+            char[] chs = new char[] { 'B', 'K', 'L', 'A', 'E' };
+            ArrayList charlist = new ArrayList();
+            for (int i = 0; i < 5; i++)
+                charlist.Add(chs[i]);
+            Console.WriteLine("Char list:");
+
+            // Use enumerator to access list
+            IEnumerator etr = charlist.GetEnumerator();
+            while (etr.MoveNext())
+                Console.Write(etr.Current + " ");
+            Console.WriteLine();
+
+            //charlist.Sort();//if we modify the collection we have to use another enumerator
+            // Re–enumerate the list
+            etr.Reset();
+            while (etr.MoveNext())
+                Console.Write(etr.Current + " ");
+            Console.WriteLine();
+
+        }
+    }
+}
