ef4d4240d3e3a4ec56da225872b99b7d952471f3
Svitlana Pustova Serebriakova <svitlana.pustova@gmail.com>
2020-10-08 23:35:23 +0300

    DictionaryEnumeratorExample

---
+++ DictionaryEnumeratorExample/DictionaryEnumeratorExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace DictionaryEnumeratorExample
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+           // Create a hash table
+           Hashtable ht = new Hashtable();
+           // Add elements to the table
+           ht.Add("Tom", "555–3456");
+           ht.Add("Mary", "555–9876");
+           ht.Add("Todd", "555–3452");
+           ht.Add("Ken", "555–7756");
+           // Demonstrate enumerator
+           IDictionaryEnumerator etr = ht.GetEnumerator();
+           Console.WriteLine("Display info using Entry.");
+           while (etr.MoveNext())
+                Console.WriteLine(etr.Entry.Key + ": " +
+                                  etr.Entry.Value);
+           Console.WriteLine();
+           Console.WriteLine("Display info using Key and Value directly.");
+           etr.Reset();
+           while (etr.MoveNext())
+                Console.WriteLine(etr.Key + ": " +  etr.Value);
+
+        }
+    }
+}
